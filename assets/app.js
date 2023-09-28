236bd374b9622659a3679747f205f93a1ec9a673
x <x@x.x>
2023-09-28 10:14:24 +0200

    add markmap

--- assets/app.js
+++ assets/app.js
@@ -6,4 +6,66 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+import { Transformer } from 'markmap-lib';
+import { Markmap, loadCSS, loadJS } from 'markmap-view';
+
+// console.log(document.querySelector("#markmap"));
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const app = new App();
+});
+
+class App {
+
+    /**
+     * @Type string
+     */
+    initialMD;
+
+    /**
+     * @Type Transformer
+     */
+    transformer;
+
+    constructor() {
+
+        this.initMD();
+        let md = this.md.value;
+
+        const { root, features } = this.transformer.transform(md);
+        const { styles, scripts } = this.transformer.getUsedAssets(features);
+
+        if (styles) loadCSS(styles);
+        if (scripts) loadJS(scripts, { getMarkmap: () => markmap });
+
+        const svgEl = document.querySelector('#svg');
+        this.mm = Markmap.create(svgEl, undefined, root);
+
+        document.querySelector('#editor').addEventListener("keyup", (e) => {
+            console.log("keyup")
+            this.update();
+        });
+
+    }
+
+    initMD() {
+        this.transformer = new Transformer();
+        this.md = document.querySelector("#editor");
+    }
+
+    update() {
+        let md = this.md.value;
+
+        const { root, features } = this.transformer.transform(md);
+        const { styles, scripts } = this.transformer.getUsedAssets(features);
+
+        if (styles) loadCSS(styles);
+        if (scripts) loadJS(scripts, { getMarkmap: () => markmap });
+
+        this.mm.setData(root);
+        // this.mm.fit();
+
+    }
+
+}
