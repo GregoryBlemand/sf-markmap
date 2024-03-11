f0b4130d9b7eda2b919d1ca249c2817ce644aefb
GregB <gregoryvalence@gmail.com>
2024-03-11 07:42:19 +0100

    granted

--- src/Controller/MarkmapController.php
+++ src/Controller/MarkmapController.php
@@ -12,8 +12,10 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/', name: "markmap_")]
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
 class MarkmapController extends AbstractController
 {
     public function __construct(
